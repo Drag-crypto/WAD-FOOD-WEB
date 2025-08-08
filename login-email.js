f9b3f7acadc0cdeca8213e6ba1feb39bf4290405
Drag-crypto <bblockscubius20@gmail.com>
2025-08-08 08:19:02 +0100

    Update login-email.js

--- login-email.js
+++ login-email.js
@@ -1,6 +1,6 @@
 const supabaseUrl = 'https://rvlealemvurgmpflajbn.supabase.co';
     const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2bGVhbGVtdnVyZ21wZmxhamJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjEwMDEsImV4cCI6MjA3MDEzNzAwMX0.TPmel2qGoG5R_hnFAB_pF9ZQob5wMkBhJVPbcqs9q8M';
-    const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+    const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
     async function login() {
       const email = document.getElementById("email").value;
@@ -29,4 +29,5 @@ const supabaseUrl = 'https://rvlealemvurgmpflajbn.supabase.co';
     document.getElementById("password").addEventListener('keypress', (e) => {
       if (e.key === 'Enter') login();
     });
+
 
