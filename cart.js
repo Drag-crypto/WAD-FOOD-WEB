33108fea2f4623dd18e8c4c5354d9e40430c8429
Drag-crypto <bblockscubius20@gmail.com>
2025-08-13 16:30:04 +0100

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,7 +1,5 @@
 // ===== Supabase Client Setup (shared) =====
-const supabaseUrl = 'https://rvlealemvurgmpflajbn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2bGVhbGVtdnVyZ21wZmxhamJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NjEwMDEsImV4cCI6MjA3MDEzNzAwMX0.TPmel2qGoG5R_hnFAB_pF9ZQob5wMkBhJVPbcqs9q8M';
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+
 
 // ===== NAVIGATION =====
 function goToCart() { window.location.href = 'Cart.html'; }
@@ -152,3 +150,4 @@ supabaseClient.auth.onAuthStateChange(async (event) => {
     await loadCartItems();
   }
 });
+
